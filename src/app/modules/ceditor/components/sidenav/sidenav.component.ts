47c1a515bd1bfe87071255d840288c9440220053
Omar Desogus <omardesogus9@gmail.com>
2019-04-04 16:09:59 +0200

    [feat] add confirm dialog for cached gist files in the navigation sidenav

--- src/app/modules/ceditor/components/sidenav/sidenav.component.ts
+++ src/app/modules/ceditor/components/sidenav/sidenav.component.ts
@@ -3,6 +3,7 @@ import {EditorService} from '../../../../core/services/editor/editor.service'
 import {Location} from '@angular/common'
 import {GistService} from '../../../../core/services/gist/gist.service'
 import {StorageService} from '../../../../core/services/storage/storage.service'
+import {UtilsService} from '../../../../core/services/utils/utils.service'
 
 @Component({
   selector: 'app-sidenav',
@@ -17,6 +18,7 @@ export class SidenavComponent implements OnInit {
 
   constructor (private editorService: EditorService,
                private gistService: GistService,
+               private utilsService: UtilsService,
                private storageService: StorageService,
                private location: Location) {
   }
@@ -28,12 +30,31 @@ export class SidenavComponent implements OnInit {
     this.currentGistFile = this.gistService.getFile()
   }
 
-  public setGistFile (gistFile: any) {
-    this.currentGistFile = gistFile
+  public async setGistFile (gistFile: any) {
     const cachedCode = this.storageService.get(`${this.gist.id}/${gistFile.filename}`)
 
+    if (cachedCode) {
+      const result = await this.utilsService.createDialog({
+        title: 'Cached code',
+        message: 'There is local saved code for this file, do you want to use it?',
+        buttons: ['No thanks', 'Ok']
+      })
+
+      switch (result) {
+        case 0:
+          this.storageService.remove(`${this.gist.id}/${gistFile.filename}`)
+          this.editorService.setCode(gistFile.content)
+          break
+        case 1:
+          this.editorService.setCode(cachedCode)
+          break
+      }
+    } else {
+      this.editorService.setCode(gistFile.content)
+    }
+
+    this.currentGistFile = gistFile
     this.gistService.setFile(gistFile.filename)
-    this.editorService.setCode(cachedCode || gistFile.content)
     this.location.replaceState(`/${this.gist.id}/${gistFile.filename}`)
   }
 
