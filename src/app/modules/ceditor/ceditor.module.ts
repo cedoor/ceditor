6f11d2877fbc975530dd334183d99262cfc25ef0
Omar Desogus <omardesogus9@gmail.com>
2019-04-06 12:14:10 +0200

    [feat] create about component

--- src/app/modules/ceditor/ceditor.module.ts
+++ src/app/modules/ceditor/ceditor.module.ts
@@ -2,22 +2,28 @@ import {NgModule} from '@angular/core'
 import {SharedModule} from '../../shared/shared.module'
 import {ToolbarComponent} from './components/toolbar/toolbar.component'
 import {EditorComponent} from './components/editor/editor.component'
-import {MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material'
+import {MatCardModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material'
 import {CeditorComponent} from './ceditor.component'
 import {SidenavComponent} from './components/sidenav/sidenav.component'
+import {AboutComponent} from './components/about/about.component'
 
 @NgModule({
   imports: [
     SharedModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   declarations: [
     CeditorComponent,
     ToolbarComponent,
     EditorComponent,
-    SidenavComponent
+    SidenavComponent,
+    AboutComponent
+  ],
+  entryComponents: [
+    AboutComponent
   ]
 })
 export class CeditorModule {
